564b53c25c92427245987942de04469febc7e167
Cyrus Studying <cyrustudying@gmail.com>
2025-06-09 17:54:52 -0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -22,4 +22,4 @@ const analytics = getAnalytics(app);
 const auth = getAuth(app);
 const db = getDatabase(app); 
 
-export { app, analytics, auth, db }; // **Remova ref daqui**
+export { app, analytics, auth, db, getDatabase }; // **Remova ref daqui**
